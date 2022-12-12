812e8f34958274ba264395a6269478d5e7b37983
Brad Williams <bradley_williams2003@hotmail.com>
2022-12-12 10:01:56 -0400

    Added 2 more seed files and 2 more tables

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,4 +1,4 @@
-CREATE TABLE cohorts(
+CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
@@ -15,3 +15,4 @@ CREATE TABLE students (
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
+
