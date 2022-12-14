38b562af355287e5ba26a8c52de256fb9fa0a633
Brad Williams <bradley_williams2003@hotmail.com>
2022-12-14 17:44:02 -0400

    added SQL injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [process.argv[2]];
+
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name='${process.argv[2].toUpperCase()}'
+WHERE cohorts.name= $1
 ORDER BY teacher;
-`)
+`, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
